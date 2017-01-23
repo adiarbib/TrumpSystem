f6883c8449e99bca4d24068e7e1e1c460fc1b10b
adiarbib <adiarbib@gmail.com>
2017-01-23 21:45:03 +0200

    hi

--- TrumpSystem/TrumpSystem/Program.cs
+++ TrumpSystem/TrumpSystem/Program.cs
@@ -10,42 +10,88 @@ namespace TrumpSystem
     class Program
     {
         const string connection_string = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\Documents\\GitHub\\TrumpSystem\\TrumpSystem\\TrumpSystem\\TrumpMe.mdf;Integrated Security=True";
-        const string name_of_table1 = "Fake News";
-        const string name_of_table2 = "Reporters";
+        const string fakeNewsTableName = "Fake_News";
+        const string reportersTableName = "Reporters";
 
         static void Main(string[] args)
         {
             SqlConnection connect = new SqlConnection(connection_string);
             connect.Open();
-            CreateTable(connect);
-            /*string answer = "";
+            string answer = "";
             while(!answer.Equals("quit"))
             {
                 Console.WriteLine("What would you like to do, Mr. Trump?");
                 answer =Console.ReadLine();
-                if (answer.Equals("list reporters"))
+                switch (answer)
                 {
+                    case "list reporters":
+                        PrintAllListReporters(connect);
+                        break;
+                    case "insert reporter":
+                        NewReporter(connect);
+                        break;
+                    case "insert news":
+                        InsertNewFakeNews(connect);
+                        break;
 
                 }
-                //CreateTable(connect);
-                //Insert(connect, "Bohemian Rhapsody", "Queen", 1975);
-                //Insert(connect, "Do I Wanna Know", "Arctic Monkeys", 2013);
-                //Insert(connect, "Dancing Shoes", "Arctic Monkeys", 2006);
-                //Insert(connect, "Wolves of Winter", "Biffy Clyro", 2016);
-                //Select(connect);
-            }*/
+                
+            }
             connect.Close();
         }
 
-        public static void CreateTable(SqlConnection connect)
+        private static void InsertNewFakeNews(SqlConnection connect)
+        {
+            
+        }
+
+        private static void NewReporter(SqlConnection connect)
+        {
+            Console.WriteLine("Please enter the reporter's first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter the reporter's last name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Please enter the reporter's address");
+            string address = Console.ReadLine();
+            InsertNewReporter(connect, firstName, lastName, address);
+        }
+
+        private static void InsertNewReporter(SqlConnection connect,string firstName, string lastName, string address)
+        {
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "INSERT INTO "+ reportersTableName + " VALUES ('"+firstName+"','"+lastName+"','"+address+"');";
+            command.ExecuteNonQuery();
+        }
+
+        public static void CreateReportersTable(SqlConnection connect)
+        {
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "CREATE TABLE " + reportersTableName + " (Id int IDENTITY(1,1) PRIMARY KEY,FirstName varchar(255),LastName varchar(255),Address varchar(255));";
+            command.ExecuteNonQuery();
+        }
+
+        private static void CreateFakeNewsTable(SqlConnection connect)
         {
             SqlCommand command = new SqlCommand();
             command.Connection = connect;
-     
-            command.CommandText = "CREATE TABLE " + name_of_table2 + " (Id IDENTITY(1,1) int PRIMARY KEY,FirstName varchar(255),LastName varchar(255),Address varchar(255));";
+            command.CommandText = "CREATE TABLE " + fakeNewsTableName + " (Id int IDENTITY(1,1) PRIMARY KEY,Title varchar(255),ReporterId int FOREIGN KEY REFERENCES Reporters(Id));";
             command.ExecuteNonQuery();
-            command.CommandText = "CREATE TABLE " + name_of_table1 + " (Id IDENTITY(1,1) int PRIMARY KEY,Title varchar(255),ReporterId int FOREIGN KEY REFERENCES Reporters(Id));";
-            command.ExecuteNonQuery();
+        }
+
+        private static void PrintAllListReporters(SqlConnection connect)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connect;
+            command.CommandText = "SELECT * FROM " + reportersTableName + ";";
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader.GetInt32(0));
+                Console.WriteLine(reader.GetString(1));
+                Console.WriteLine(reader.GetString(2));
+                Console.WriteLine(reader.GetString(3));
+            }
+            reader.Close();
         }
     }
 }
